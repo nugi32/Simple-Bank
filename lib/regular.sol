4d9e85eef000320c665845d5f14a9cdea513ebdd
nugi <nugrohoadhipratama135@gmail.com>
2025-10-04 14:31:22 +0700

    Delete lib directory

--- lib/regular.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-library regular {
-    function greater_than_0(uint num) internal pure {
-        require(num > 0, "Must be greater than 0 !");
-    }
-}
